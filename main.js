bccb4fd1f3d4be5fc12a1e07cd80aab54025b9e6
Juan_Bantar <juan.felipe.2001@hotmail.com>
2022-12-21 15:57:13 -0500

    minuto 47:16
    https://www.youtube.com/watch?v=idRdAgNnC5Q&t=2875s

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById8("btn_open").addEventListener("click", open_close_menu);
+
+var side_menu = document.getElementById("side_menu");
+var btn_open = document.getElementById9("btn_open");
+var body = ducument.getElementById("body");
+
+function open_close_menu() {
+  body.classlist.toggle("body move");
+  menu_side.classlist.toogle("menu__side_move");
+}
