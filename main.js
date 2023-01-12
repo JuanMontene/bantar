ae6577d2249c706c7ec9c7e4a0819773f3708d35
Juan_Bantar <juan.felipe.2001@hotmail.com>
2023-01-12 15:57:44 -0500

    Comienzo nosotros

--- main.js
+++ main.js
@@ -1,10 +1,60 @@
-document.getElementById8("btn_open").addEventListener("click", open_close_menu);
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav_menu');
 
-var side_menu = document.getElementById("side_menu");
-var btn_open = document.getElementById9("btn_open");
-var body = ducument.getElementById("body");
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('nav_menu_visible');
+});
 
-function open_close_menu() {
-  body.classlist.toggle("body move");
-  menu_side.classlist.toogle("menu__side_move");
+/* CODIGO PARA SLIDE DE INFORMACION DE TARJETAS */
+
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n) {
+  muestraSlides((indice += n));
 }
+
+function posicionSlide(n) {
+  muestraSlides((indice = n));
+}
+
+setInterval(function tiempo() {
+  muestraSlides((indice += 1));
+}, 4000);
+
+function muestraSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mi-slider');
+  let barras = document.getElementsByClassName('barra');
+
+  if (n > slides.length) {
+    indice = 1;
+  }
+
+  if (n < 1) {
+    indice = slides.length();
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (i = 0; i < barras.length; i++) {
+    barras[i].className = barras[i].className.replace('active', '');
+  }
+
+  slides[indice - 1].style.display = 'block';
+  barras[indice - 1].className += ' active';
+}
+// document.getElementById("btn_open").addEventListener("click", open_close_menu);
+
+// // poder que el body se mueva hacia la derecha
+// var side_menu = document.getElementById("nav_side");
+// var btn_open = documento.getElementById("btn_open");
+// var body = document.getElementById("body");
+
+// // Evento para mostrar y ocultar menu
+// function open_close_menu() {
+//   body.classList.toggle("body_move");
+//   side_menu.classList.toggle("menu_side_move");
+// }
