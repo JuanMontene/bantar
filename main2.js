4c8b1635baf17efd1e6b05b3e31ffd0095ee7e38
JuanMontene <juan.felipe.2001@hotmail.com>
2023-04-28 18:09:42 -0500

    FISNIH PAGE SERVICES

--- main2.js
+++ main2.js
@@ -1,10 +1,432 @@
-const svgIcon = document.getElementById('icon-link');
-const listItem = document.querySelector('.questions-list_item1');
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
 
-listItem.addEventListener('click', () => {
-  if (svgIcon.getAttribute('href') === './img/icons.svg#icon-plus') {
-    svgIcon.setAttribute('href', './img/icons.svg#icon-minus');
-  } else {
-    svgIcon.setAttribute('href', './img/icons.svg#icon-plus');
+  var plusIcon = document.querySelector('.plus-icon');
+  var minusIcon = document.querySelector('.minus-icon');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info')) {
+    var dropdowns = document.getElementsByClassName('button-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+
+        var plusIcon = document.querySelector('.plus-icon');
+        var minusIcon = document.querySelector('.minus-icon');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
   }
-});
+};
+
+/* CODE FOR SECOND BOX SERVICES INFORMATION */
+function myFunction1() {
+  document.getElementById('myDropdown1').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon1');
+  var minusIcon = document.querySelector('.minus-icon1');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info1')) {
+    var dropdowns = document.getElementsByClassName('button-content1');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+
+        var plusIcon = document.querySelector('.plus-icon1');
+        var minusIcon = document.querySelector('.minus-icon1');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/*CUANTO TIEMPO SE DEMORA*/
+function myFunction2() {
+  document.getElementById('myDropdown2').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon2');
+  var minusIcon = document.querySelector('.minus-icon2');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info2')) {
+    var dropdowns = document.getElementsByClassName('button-content2');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon2');
+        var minusIcon = document.querySelector('.minus-icon2');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* FLASH CLEANING - A QUIEN VA DIRIGIDA ESTA LIMPIEZA */
+function myFunction3() {
+  document.getElementById('myDropdown3').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon3');
+  var minusIcon = document.querySelector('.minus-icon3');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info3')) {
+    var dropdowns = document.getElementsByClassName('button-content3');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon3');
+        var minusIcon = document.querySelector('.minus-icon3');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* FLASH CLEANING - CADA CUANTO SE DEBE HACER ESTA LIMPIEZA  */
+function myFunction4() {
+  document.getElementById('myDropdown4').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon4');
+  var minusIcon = document.querySelector('.minus-icon4');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info4')) {
+    var dropdowns = document.getElementsByClassName('button-content4');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon4');
+        var minusIcon = document.querySelector('.minus-icon4');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* FLASH CLEANING - CUANTO SE DEMORA ESTA LIMPIEZA */
+function myFunction5() {
+  document.getElementById('myDropdown5').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon5');
+  var minusIcon = document.querySelector('.minus-icon5');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info5')) {
+    var dropdowns = document.getElementsByClassName('button-content5');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon5');
+        var minusIcon = document.querySelector('.minus-icon5');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* HIDRATACIÓN FACIAL - QUE TIPO DE PIELES PUEDEN HACERSE LA DIRATACIÓN */
+function myFunction6() {
+  document.getElementById('myDropdown6').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon6');
+  var minusIcon = document.querySelector('.minus-icon6');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info6')) {
+    var dropdowns = document.getElementsByClassName('button-content6');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon6');
+        var minusIcon = document.querySelector('.minus-icon6');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* HIDRATACIÓN FACIAL - CUANTO TIEMPO DURA EL EFECTO DE ESTE TRATAMIENTO */
+function myFunction7() {
+  document.getElementById('myDropdown7').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon7');
+  var minusIcon = document.querySelector('.minus-icon7');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info7')) {
+    var dropdowns = document.getElementsByClassName('button-content7');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon7');
+        var minusIcon = document.querySelector('.minus-icon7');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* HIDRATACIÓN FACIAL - CUANTO TIEMPO DURA EL EFECTO DE ESTE TRATAMIENTO */
+function myFunction8() {
+  document.getElementById('myDropdown8').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon8');
+  var minusIcon = document.querySelector('.minus-icon8');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info8')) {
+    var dropdowns = document.getElementsByClassName('button-content8');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon8');
+        var minusIcon = document.querySelector('.minus-icon8');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - PARA QUIEN VA DIRIGIDO ESTE PROTOCOLO */
+function myFunction9() {
+  document.getElementById('myDropdown9').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon9');
+  var minusIcon = document.querySelector('.minus-icon9');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info9')) {
+    var dropdowns = document.getElementsByClassName('button-content9');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon9');
+        var minusIcon = document.querySelector('.minus-icon9');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - QUE RESULTADOS SE OBTIENEN DE ESTE PROCEDIMIENTO */
+function myFunction10() {
+  document.getElementById('myDropdown10').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon10');
+  var minusIcon = document.querySelector('.minus-icon10');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info10')) {
+    var dropdowns = document.getElementsByClassName('button-content10');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon10');
+        var minusIcon = document.querySelector('.minus-icon10');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - ESTE PROCEDIMIENTO GENERA DOLOR O ENROJECIMIENTO */
+function myFunction11() {
+  document.getElementById('myDropdown11').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon11');
+  var minusIcon = document.querySelector('.minus-icon11');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info11')) {
+    var dropdowns = document.getElementsByClassName('button-content11');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon11');
+        var minusIcon = document.querySelector('.minus-icon11');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - QUE RESULTADOS SE OBTIENEN DE ESTE PROCEDIMIENTO */
+function myFunction12() {
+  document.getElementById('myDropdown12').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon12');
+  var minusIcon = document.querySelector('.minus-icon12');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info12')) {
+    var dropdowns = document.getElementsByClassName('button-content12');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon12');
+        var minusIcon = document.querySelector('.minus-icon12');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - QUE RESULTADOS SE OBTIENEN DE ESTE PROCEDIMIENTO */
+function myFunction13() {
+  document.getElementById('myDropdown13').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon13');
+  var minusIcon = document.querySelector('.minus-icon13');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info13')) {
+    var dropdowns = document.getElementsByClassName('button-content13');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon13');
+        var minusIcon = document.querySelector('.minus-icon13');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - CADA CUANTO DEBO REALIZAR ESTE PROCEDIMIENTO */
+function myFunction14() {
+  document.getElementById('myDropdown14').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon14');
+  var minusIcon = document.querySelector('.minus-icon14');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info14')) {
+    var dropdowns = document.getElementsByClassName('button-content14');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon14');
+        var minusIcon = document.querySelector('.minus-icon14');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
+
+/* MICRODERMOABRASION - CUANTO TIEMPO DURA EL PROTOCOLO */
+function myFunction15() {
+  document.getElementById('myDropdown15').classList.toggle('show');
+
+  var plusIcon = document.querySelector('.plus-icon15');
+  var minusIcon = document.querySelector('.minus-icon15');
+  plusIcon.classList.toggle('hidden');
+  minusIcon.classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.drop-info15')) {
+    var dropdowns = document.getElementsByClassName('button-content15');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        var plusIcon = document.querySelector('.plus-icon15');
+        var minusIcon = document.querySelector('.minus-icon15');
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('show');
+      }
+    }
+  }
+};
